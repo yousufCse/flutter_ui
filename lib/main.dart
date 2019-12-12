dc80a13100c9eb433c5a55b490350626d6113004
Md. Yousuf Ali <myali@yousuf-MacBook-Pro.local>
2019-12-12 16:37:24 +0600

    Created talent hire ui

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -24,39 +25,227 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: Icon(
+          Icons.forum,
+          color: Colors.green,
+          size: 30.0,
         ),
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+          color: Colors.grey,
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu),
+            color: Colors.green,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment(0.0, -0.40),
+                height: 100.0,
+                color: Colors.white,
+                child: Text(
+                  'Get coaching',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(25.0, 90.0, 25.0, 0.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(blurRadius: 2.0, color: Colors.grey),
+                  ],
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25.0, 25.0, 5.0, 5.0),
+                          child: Text(
+                            'YOU HAVE',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25.0, 40.0, 5.0, 25.0),
+                          child: Text(
+                            '206',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 100.0),
+                    Container(
+                      height: 50.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                          color: Colors.greenAccent[100],
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Center(
+                        child: Text(
+                          'Buy more',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.green),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40.0),
+          Container(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'MY COACHES',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0),
+                ),
+                Text(
+                  'VIEW PAST SESSIONS',
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0),
+          GridView.count(
+            crossAxisCount: 2,
+            primary: false,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 4.0,
+            shrinkWrap: true,
+            children: <Widget>[
+              _buildCard('Md. Yousuf Ali', 'Available', 1),
+              _buildCard('Md. Yousuf Ali', 'Away', 2),
+              _buildCard('Md. Yousuf Ali', 'Away', 3),
+              _buildCard('Md. Yousuf Ali', 'Available', 4),
+              _buildCard('Md. Yousuf Ali', 'Available', 5),
+              _buildCard('Md. Yousuf Ali', 'Away', 6),
+              _buildCard('Md. Yousuf Ali', 'Away', 7),
+              _buildCard('Md. Yousuf Ali', 'Available', 8),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildCard(String name, String status, int cardIndex) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 7.0,
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 12.0),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Colors.green,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        // 'https://qph.fs.quoracdn.net/main-qimg-b77900459f7ed1e31776c6fc42e65eaf'),
+                        'http://172.16.205.211/uploads/-/system/user/avatar/14/yousuf_def.jpg?width=400'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 45.0),
+                height: 20.0,
+                width: 20.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: status == 'Away' ? Colors.amber : Colors.green,
+                  border: Border.all(
+                      color: Colors.white,
+                      style: BorderStyle.solid,
+                      width: 2.0),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 15.0),
+          Text(
+            name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15.0,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            status,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12.0,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Expanded(
+            child: Container(
+              // width: 175.0,
+              decoration: BoxDecoration(
+                color: status == 'Away' ? Colors.grey : Colors.green,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+              ),
+              child: Center(
+                child: Text('Request', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+      margin: cardIndex.isEven
+          ? EdgeInsets.fromLTRB(10.0, 0.0, 25.0, 10.0)
+          : EdgeInsets.fromLTRB(25.0, 0.0, 5.0, 10.0),
     );
   }
 }
