6f14767b782eda4027d8382e2e541bbca9ec2015
Md. Yousuf Ali <yousuf.stu@gmail.com>
2019-12-20 15:44:28 +0600

    Created gallery for restuarant app

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -17,6 +17,13 @@ class _HomeScreenState extends State<HomeScreen> {
     'images/d.jpg'
   ];
 
+  final galleryList = [
+    {'name': 'French Fry', 'image': 'images/a.jpg', 'off': '40'},
+    {'name': 'Burger', 'image': 'images/b.jpg', 'off': '25'},
+    {'name': 'Pizza', 'image': 'images/c.jpg', 'off': '45'},
+    {'name': 'Chicken', 'image': 'images/d.jpg', 'off': '10'},
+  ];
+
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
@@ -60,7 +67,8 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget _buildFoodGallery(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      height: 260,
+      margin: EdgeInsets.only(top: 20.0),
+      height: 240,
       width: screenWidth,
       child: PageView(
         controller: controller,
@@ -72,18 +80,57 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Widget> gallery() {
     List<Widget> items = List<Widget>();
 
-    for (int i = 0; i < images.length; i++) {
+    for (int i = 0; i < galleryList.length; i++) {
       items.add(Padding(
         padding: EdgeInsets.all(10.0),
         child: Container(
           child: Stack(
             fit: StackFit.expand,
             children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 5.0,
+                    ),
+                  ],
+                ),
+              ),
               ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(20.0)),
                 child: Image.asset(
-                  images[i],
-                  fit: BoxFit.contain,
+                  galleryList[i]['image'],
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [Colors.black, Colors.transparent],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, bottom: 15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      galleryList[i]['name'],
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    SizedBox(height: 5.0,),
+                    Text(
+                      'Upto ${galleryList[i]['off']}% discount',
+                      style: TextStyle(color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.bold),
+                    )
+                  ],
                 ),
               ),
             ],
