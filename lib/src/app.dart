2f030416601a0b0bdf473609db4f22f3cd644c33
Md. Yousuf Ali <yousuf.stu@gmail.com>
2019-12-22 23:56:52 +0600

    Created some screen for shimmer

---
+++ lib/src/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'screens/lock_screen.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Shimmer example', home: LockScreen());
+  }
+}
