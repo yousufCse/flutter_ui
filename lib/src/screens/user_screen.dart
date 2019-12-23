94851b498f6ba62797e3f8727089b4bc0e89da64
Md. Yousuf Ali <myali@yousuf-MacBook-Pro.local>
2019-12-23 13:35:51 +0600

    Shimmer for profile picture

--- lib/src/screens/user_screen.dart
+++ lib/src/screens/user_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:shimmer/shimmer.dart';
 
 class UserScreen extends StatefulWidget {
   @override
@@ -14,10 +14,65 @@ class _UserScreenState extends State<UserScreen> {
         title: Text('User Information'),
       ),
       body: Container(
-        child: Center(
-          child: Text('User info screen'),
+        child: ListView(
+          children: <Widget>[
+            _buildProfilePicture(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildProfilePicture() {
+    return Container(
+      padding: EdgeInsets.only(top: 30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _buildShimmer(
+            child: Image(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.red,
+              image: AssetImage('images/user.png'),
+            ),
+          ),
+          _buildShimmer(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Md. Yousuf Ali',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.black
+                ),
+              ),
+            ),
+          ),
+          _buildShimmer(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'yousuf.csebd@gmail.com',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                  backgroundColor: Colors.green
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildShimmer({Widget child}) {
+    return Shimmer.fromColors(
+      baseColor: Colors.black87,
+      highlightColor: Colors.grey,
+      child: child,
     );
   }
 }
