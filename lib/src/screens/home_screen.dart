93fc01cd24b98502ddeeaa3462694de894f8ac32
Md. Yousuf Ali <myali@yousuf-MacBook-Pro.local>
2019-12-27 14:25:06 +0600

    Instanstiate animation object

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  Animation catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Animation'),
+      ),
+      body: buildAnimation(),
     );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
   }
 }
