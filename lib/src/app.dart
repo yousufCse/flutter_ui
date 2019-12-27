3e9284a641ceeb48063eb63295e8254e3430739e
Md. Yousuf Ali <myali@yousuf-MacBook-Pro.local>
2019-12-27 11:57:01 +0600

    Structured folder

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
