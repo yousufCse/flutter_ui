300247d67b5c545ae4f95b336a6de54dd4005e78
Md. Yousuf Ali <yousuf.stu@gmail.com>
2020-01-03 21:04:53 +0600

    Created some dart file for localizations

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/src/locale/app_translation_delegate.dart';
 import 'screens/login_screen.dart';
+
 
 class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        const AppTranslationsDelegate(),
+        
+
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('bn', '')
+      ],
       title: "Login",
       home: LoginScreen(),
     );
